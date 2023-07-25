f00dcef1bfe5102e2fa4965d0e2f98537fa8d7a5
SkylerC <122322436+Vonglory176@users.noreply.github.com>
2023-07-25 16:26:46 -0700

    Initial upload of working version

---
+++ main.js
@@ -0,0 +1,33 @@
+import { imageGenerate } from "./generateRequests/imageGenerate.js"
+import { checkGenerate } from "./generateRequests/checkGenerate.js"
+import { statusGenerate } from "./generateRequests/statusGenerate.js"
+
+const apiKey = "L15qrkaHUZU7qbAUlkIlXA"
+
+$("#generate-button").on("click", generateButtonClicked)
+
+async function generateButtonClicked() {
+    let input = $("#prompt-input").val().trim()
+    console.log(input)
+
+    if (input) {
+        $("#generate-button").attr("disabled", "true")
+        let imageData = await imageGenerate(apiKey, input)
+        
+        let interval, checkData, counter = 0
+        interval = setInterval(async () => {
+            checkData = await checkGenerate(imageData.id)
+            $("#timer span").html(counter++)
+    
+            try {
+                if(checkData.done) {
+                    clearInterval(interval)
+                    let statusData = await statusGenerate(imageData.id)
+                    $("#output-img").attr("src",`${statusData.generations[0].img}`)
+                    $("#generate-button").removeAttr("disabled")
+                }
+            } catch(error) {console.log(error)}
+        }, 1000)
+    }
+    else alert("Please enter a legitimate prompt")
+}
